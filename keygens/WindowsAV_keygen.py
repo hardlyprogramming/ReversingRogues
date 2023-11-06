2da3150d494979da3254ae0390a1ffe869ca4a96
hardlyprogramming <138535112+hardlyprogramming@users.noreply.github.com>
2023-11-06 12:36:25 -0600

    Update WindowsAV_keygen.py

--- keygens/WindowsAV_keygen.py
+++ keygens/WindowsAV_keygen.py
@@ -1,1 +1,21 @@
+import random
 
+# useable letters and numbers (No 'F, C, A, E')
+chars = 'BDGHIJKLMNOPQRSTUVWXYZ'
+
+# useable numbers for the last character of serial
+nums = '123'
+
+# Serial Format:  *F** - ***C - **A* - E**1
+
+x = '*F**-***C-**A*-E**$'
+serial = ''
+
+for char in x:
+    if char == '*':
+        char = random.choice(chars)
+    if char  == '$':
+        char = random.choice(nums)
+    serial += char
+    
+print(serial)
